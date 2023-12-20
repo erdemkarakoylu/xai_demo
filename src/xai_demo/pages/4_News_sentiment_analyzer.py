85d42d82214e3525c26f0a3ecbd343f8a7792d0a
Erdem Karakoylu <ekarakoylu@researchinnovations.com>
2023-12-20 10:04:12 -0500

    added streamlit components

--- src/xai_demo/pages/4_News_sentiment_analyzer.py
+++ src/xai_demo/pages/4_News_sentiment_analyzer.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 import loguru
+import matplotlib.pyplot as pp
 import shap
 import streamlit as st
+import streamlit.components.v1 as components
 import transformers
 from transformers_interpret import SequenceClassificationExplainer
 
@@ -10,6 +12,7 @@ PROJECT_PATH = Path.cwd().parent.parent
 MODEL_PATH = PROJECT_PATH / 'model_assets'
 ASSETS_PATH = PROJECT_PATH / 'assets'
 model_path = MODEL_PATH / 'transformer_model'
+
 
 def run_prediction(input):
     prediction = classifier(input)[0]
@@ -27,25 +30,40 @@ def run_explainer(input, pred_label):
     explainer = shap.Explainer(classifier)
     shap_values = explainer([input])
     shap_vals_4_lbl = shap_values[:, :, pred_label]
-    shap.plot.text(shap_vals_4_lbl)
+    return shap.plots.text(shap_vals_4_lbl, display=False)
 
 
-classifier = transformers.pipeline(
-    model=model_path, task='sentiment-analysis')
+def st_shap(plot, height=800):
+    #shap_html = f"<head>{shap.getjs()}</head><body{plot.html()}</body>"
+    shap_html = f"<head>{shap.getjs()}</head><body{plot}</body>"
+    components.html(shap_html, height=height)
+
+@st.cache_resource
+def load_classifier(model_path):
+    classifier = transformers.pipeline(
+        model=model_path, task='sentiment-analysis')
+    return classifier
+
+shap.initjs()
 
 
+
+classifier = load_classifier(model_path)
 text = st.text_area(
-        "Enter text to be interpreted",
+        "Enter text for evaluation",
         "I like you, I love you",
-        height=200,
-        max_chars=500,
+        height=40,
+        max_chars=200,
     )
-columns = st.columns([1, 3])
-with columns[0]:
-    if st.button("Predict Sentiment"):  
-        prediction = run_prediction(text)
-with columns[1]:
-    if st.button("Interpret Result"):
-        run_explainer(text, prediction['label'])
+predict = st.button("Predict & Interpret...")
 
+if predict:  
+    prediction = run_prediction(text)
+    st.markdown(" ")
+    with st.spinner("Computing interpretation..."):
+
+        st_shap(run_explainer(text, prediction['label']))
+
+cls_explainer = SequenceClassificationExplainer(
+    model=classifier.model, tokenizer=classifier.tokenizer)
 
